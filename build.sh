e0a1c768d9b0e26426479b645520905cb0291a07
Christoph Theis <christoph_theis@outlook.com>
2018-10-01 17:07:55 +0200

    added virtualenv to build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 PYTHONPATH=''
+workon jenkins_with_Python
 nosetests --with-xunit --all-modules --traverse-namespace --with-coverage --cover-package=project1 --cover-inclusive
 python -m coverage xml --include=project1*
 pylint -f parseable -d I0011,R0801 project1 | tee pylint.out
