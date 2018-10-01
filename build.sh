33663b48b1aab8107594bcc31019a27537069345
Christoph Theis <christoph_theis@outlook.com>
2018-10-01 17:27:48 +0200

    Hopefully this works now

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+source virtualenvwrapper.sh
 PYTHONPATH=''
-workon jenkins_with_Python
+workon Jenkins_with_Python
 nosetests --with-xunit --all-modules --traverse-namespace --with-coverage --cover-package=project1 --cover-inclusive
-python -m coverage xml --include=project1*
+python -m coverage xml --include=project1
 pylint -f parseable -d I0011,R0801 project1 | tee pylint.out
 echo "Done!"
