bd0783b525275fc6e3c831332ea481d399c5465a
Christoph Theis <christoph_theis@outlook.com>
2018-10-04 19:25:28 +0200

    Revert authentication_tests to python2.7

--- test/authentication_tests.py
+++ test/authentication_tests.py
@@ -1,20 +1,20 @@
 from unittest import TestCase
-from unittest.mock import patch
+from mock import patch
 import project1.authentication as auth
 
 class StandAloneTests(TestCase):
 
-    @patch('builtins.open')
+    @patch('__builtin__.open')
     def test_login_success(self, mock_open):
         mock_open.return_value.read.return_value = 'george|bosco\n'
         self.assertTrue(auth.login('george', 'bosco'))
 
-    @patch('builtins.open')
+    @patch('__builtin__.open')
     def test_login_bad_creds(self, mock_open):
         mock_open.return_value.read.return_value = 'george|bosco\n'
         self.assertFalse(auth.login('george', 'marbleRye'))
 
-    @patch('builtins.open')
+    @patch('__builtin__.open')
     def test_login_error(self, mock_open):
         mock_open.side_effect = IOError()
         self.assertFalse(auth.login('george', 'bosco'))
