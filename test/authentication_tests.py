6550fee80ace265ef11c7ad39d253e120229c168
Christoph Theis <christoph_theis@outlook.com>
2018-10-04 19:51:04 +0200

    Added a bad test for logout.

--- test/authentication_tests.py
+++ test/authentication_tests.py
@@ -18,3 +18,7 @@ class StandAloneTests(TestCase):
     def test_login_error(self, mock_open):
         mock_open.side_effect = IOError()
         self.assertFalse(auth.login('george', 'bosco'))
+
+    def test_logout(self):
+        """Test the logout functio...badly."""
+        self.assertEqual(0, 1)
